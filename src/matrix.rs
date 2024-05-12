01f0f26bf992f8a4a4f86c32c2487a3e654be9cc
m104 <m104ngc4594@sina.com>
2024-05-12 11:22:46 +0800

    refactor: make matrix multiply to be concurrency with threads

--- src/matrix.rs
+++ src/matrix.rs
@@ -2,7 +2,13 @@ use anyhow::{anyhow, Result};
 use std::{
     fmt,
     ops::{Add, AddAssign, Mul},
+    sync::mpsc,
+    thread,
 };
+
+use crate::{dot_product, Vector};
+
+const NUM_THREADS: usize = 4;
 
 // [[1, 2], [1, 2], [1, 2]] => [1, 2, 1, 2, 1, 2]
 pub struct Matrix<T> {
@@ -44,6 +50,17 @@ where
     }
 }
 
+impl<T> Mul for Matrix<T>
+where
+    T: Add<Output = T> + Default + AddAssign + Copy + Mul<Output = T> + Send + 'static,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        multiply(&self, &rhs).expect("Matrix multiply error")
+    }
+}
+
 impl<T> fmt::Debug for Matrix<T>
 where
     T: fmt::Display,
@@ -53,21 +70,92 @@ where
     }
 }
 
+pub struct MsgInput<T> {
+    idx: usize,
+    row: Vector<T>,
+    col: Vector<T>,
+}
+
+impl<T> MsgInput<T> {
+    pub fn new(idx: usize, row: Vector<T>, col: Vector<T>) -> Self {
+        Self { idx, row, col }
+    }
+}
+
+pub struct MsgOutput<T> {
+    idx: usize,
+    value: T,
+}
+
+pub struct Msg<T> {
+    input: MsgInput<T>,
+    // sender to send the result back
+    sender: oneshot::Sender<MsgOutput<T>>,
+}
+
+impl<T> Msg<T> {
+    pub fn new(input: MsgInput<T>, sender: oneshot::Sender<MsgOutput<T>>) -> Self {
+        Self { input, sender }
+    }
+}
+
 pub fn multiply<T>(a: &Matrix<T>, b: &Matrix<T>) -> Result<Matrix<T>>
 where
-    T: Add<Output = T> + Default + AddAssign + Copy + Mul<Output = T>,
+    T: Add<Output = T> + Default + AddAssign + Copy + Mul<Output = T> + Send + 'static,
 {
     if a.col != b.row {
         return Err(anyhow!("Matrix multiply error: a.col != b.row"));
     }
 
-    let mut data = vec![T::default(); a.row * b.col];
+    let senders = (0..NUM_THREADS)
+        .map(|_| {
+            let (tx, rx) = mpsc::channel::<Msg<T>>();
+            thread::spawn(move || {
+                for msg in rx {
+                    let value = dot_product(msg.input.row, msg.input.col)?;
+                    if let Err(e) = msg.sender.send(MsgOutput {
+                        idx: msg.input.idx,
+                        value,
+                    }) {
+                        eprintln!("Send error: {:?}", e);
+                    }
+                }
+                Ok::<_, anyhow::Error>(())
+            });
+            tx
+        })
+        .collect::<Vec<_>>();
+
+    // generate 4 threads which receive msg and do dot prouct
+    let matrix_len = a.row * b.col;
+    let mut data = vec![T::default(); matrix_len];
+    let mut receivers = Vec::with_capacity(matrix_len);
+
+    // map/reduce: map phase
     for i in 0..a.row {
         for j in 0..b.col {
-            for k in 0..a.col {
-                data[i * b.col + j] += a.data[i * a.col + k] * b.data[k * b.col + j];
+            let row = Vector::new(&a.data[i * a.col..(i + 1) * a.col]);
+            let col_data = b.data[j..]
+                .iter()
+                .step_by(b.col)
+                .copied()
+                .collect::<Vec<_>>();
+            let col = Vector::new(col_data);
+            let idx = i * b.col + j;
+            let input = MsgInput::new(idx, row, col);
+            let (tx, rx) = oneshot::channel();
+            let msg = Msg::new(input, tx);
+            if let Err(e) = senders[idx % NUM_THREADS].send(msg) {
+                eprintln!("Send error: {:?}", e);
             }
+            receivers.push(rx);
         }
+    }
+
+    // map/reduce: reduce phase
+    for rx in receivers {
+        let output = rx.recv()?;
+        data[output.idx] = output.value;
     }
 
     Ok(Matrix {
@@ -85,7 +173,7 @@ mod tests {
     fn test_matrix_multiply() -> Result<()> {
         let a = Matrix::new([1, 2, 3, 4, 5, 6], 2, 3);
         let b = Matrix::new([1, 2, 3, 4, 5, 6], 3, 2);
-        let c = multiply(&a, &b).unwrap();
+        let c = a * b;
         assert_eq!(c.col, 2);
         assert_eq!(c.row, 2);
         assert_eq!(c.data, vec![22, 28, 49, 64]);
@@ -98,9 +186,25 @@ mod tests {
     fn test_matrix_display() -> Result<()> {
         let a = Matrix::new([1, 2, 3, 4], 2, 2);
         let b = Matrix::new([1, 2, 3, 4], 2, 2);
-        let c = multiply(&a, &b)?;
+        let c = a * b;
         assert_eq!(c.data, vec![7, 10, 15, 22]);
         assert_eq!(format!("{}", c), "{7 10, 15 22}");
         Ok(())
+    }
+
+    #[test]
+    fn test_a_can_not_multiply_b() {
+        let a = Matrix::new([1, 2, 3, 4, 5, 6], 2, 3);
+        let b = Matrix::new([1, 2, 3, 4], 2, 2);
+        let c = multiply(&a, &b);
+        assert!(c.is_err());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_a_can_not_multiply_b_panic() {
+        let a = Matrix::new([1, 2, 3, 4, 5, 6], 2, 3);
+        let b = Matrix::new([1, 2, 3, 4], 2, 2);
+        let _ = a * b;
     }
 }
